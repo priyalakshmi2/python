f333e7bf045b3fb6a7c3455565838df9cc71fae7
priyalakshmi2 <60418753+priyalakshmi2@users.noreply.github.com>
2020-01-29 10:58:47 +0530

    Create call.py

---
+++ call.py
@@ -0,0 +1,10 @@
+def add(num1,num2):
+    return num1+num2
+def sub(num1,num2):
+    return num1-num2
+def mul(num1,num2):
+    return num1*num:
+def div(num1,num2):
+    return num1/num2
+print(select operation
+print(1.add)
